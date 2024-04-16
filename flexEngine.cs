6bd423acc2426ac2cf64d481f1e67725a299e57c
Ian Ngoh <ian@mages.edu.sg>
2024-04-16 23:32:49 +0800

    Update flexEngine.cs

--- flexEngine.cs
+++ flexEngine.cs
@@ -1,39 +1,73 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
-public static class flexEngine
+using UnityEngine.Audio;
+public class flexEngine : MonoBehaviour
 {
-    static Dictionary<string, anClipMag> _SoundBank = new Dictionary<string, anClipMag>();
-    static flexEngine()
+    public flexEngine Instance;
+    private void Awake()
     {
-        string FilePath = "AudioMags";
-        anClipMag[] m = Resources.LoadAll<anClipMag>(FilePath);
-        foreach (anClipMag b in m)
-            _SoundBank.Add(b.name, b);
-    }
-    public static anClipMag FetchMag(string SoundID)
-    {
-        if (_SoundBank.TryGetValue(SoundID, out anClipMag mag))
-            return mag;
-        else
-            throw new System.Exception(SoundID + " not found");
-    }
-    public static AudioClip FetchClip(string SoundID, out float gain)
-    {
-        if (_SoundBank.TryGetValue(SoundID, out anClipMag mag))
-            return mag.Randomise(out gain);
-        else
-            throw new System.Exception(SoundID + " not found");
-    }
-    public static AudioClip PlayClipFromSource(string SoundID, AudioSource source)
-    {
-        if (_SoundBank.TryGetValue(SoundID, out anClipMag mag))
+        if (Instance == null)
         {
-            AudioClip c = mag.Randomise(out float gain);
-            source.PlayOneShot(c, gain);
-            return c;
+            Instance = this;
+            LoadSoundBank();
+            DontDestroyOnLoad(gameObject);
         }
         else
-            throw new System.Exception(SoundID + " not found");
+        {
+            Debug.LogWarning(Instance.gameObject.name + "flexEngine Already exists");
+            Destroy(this);
+        }
+    }
+    private void OnDestroy()
+    {
+        if (Instance == this)
+            Instance = null;
+    }
+    public SfxMag[] AudioMags;
+    Dictionary<string, SfxMag> _SoundBank = new Dictionary<string, SfxMag>();
+    void LoadSoundBank()
+    {
+        foreach (var sfx in AudioMags)
+        {
+            var tag = sfx.Tag;
+            if (!_SoundBank.ContainsKey(tag))
+                _SoundBank.Add(tag, sfx);
+        }
+    }
+    public SfxMag FetchSfx(string SoundID)
+    {
+        if (_SoundBank.TryGetValue(SoundID, out SfxMag data))
+            return data;
+        else
+            throw new Exception(SoundID + " not found");
+    }
+    public AudioSource PlayClipAtPoint(string SoundId, Vector3 position)
+    {
+        SfxMag mag = FetchSfx(SoundId);
+        AudioClip clip = mag.Randomise(out float gain);
+        GameObject go = new GameObject(SoundId);
+        go.transform.position = position;
+        AudioSource source = go.AddComponent<AudioSource>();
+        source.spatialBlend = 1;
+        source.outputAudioMixerGroup = mag.Channel;
+        source.PlayOneShot(clip, gain);
+        Destroy(go, clip.length);
+        return source;
+    }
+    public class SfxMag
+    {
+        public string Tag;
+        public AudioMixerGroup Channel;
+        [Range(0, 1)]
+        public float MinRandomVolume = 1;
+        public AudioClip[] Clips;
+        public AudioClip Randomise(out float gain)
+        {
+            int key = UnityEngine.Random.Range(0, Clips.Length);
+            AudioClip c = Clips[key];
+            gain = UnityEngine.Random.Range(MinRandomVolume, 1);
+            return c;
+        }
     }
 }
