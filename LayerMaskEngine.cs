390999c33dc92b72c4f4c2ec788577a78327b1d5
Ian Ngoh <ian@mages.edu.sg>
2024-07-25 22:44:04 +0800

    Added LayerMask utility

---
+++ LayerMaskEngine.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+public static class LayerMaskEngine
+{
+    public static bool MatchLayerMask(GameObject gameObject, params string[] layerNames)
+    {
+        return MatchLayerMask(gameObject.layer, layerNames);
+    }
+    public static bool MatchLayerMask(int layer, params string[] layerNames)
+    {
+        LayerMask mask = LayerMask.GetMask(layerNames);
+        return mask.ContainsLayer(layer);
+    }
+    public static bool ContainsLayer(this LayerMask layerMask, GameObject gameObject)
+    {
+        return ContainsLayer(layerMask, gameObject.layer);
+    }
+    public static bool ContainsLayer(this LayerMask layerMask, int layer)
+    {
+        int bitLayer = 1 << layer;
+        int match = bitLayer & layerMask;
+        return match > 0;
+    }
+}
