589da4bcd99acee1d86eba32b67861c487e61f82
IanGeninco <ian_ngoh@hotmail.com>
2025-02-21 00:26:03 +0800

    Update AnimatorReceiverPassthrough.cs
    use action task instead

--- AnimatorReceiverPassthrough.cs
+++ AnimatorReceiverPassthrough.cs
@@ -1,18 +1,11 @@
 using UnityEngine;
+using System;
 [RequireComponent(typeof(Animator))]
 public class AnimatorReceiverPassthrough : MonoBehaviour
 {
-    IKeyframeReceiver _Host;
-    public void SetHost(IKeyframeReceiver host)
-    {
-        _Host = host;
-    }
+    public Action<string> OnReceiveKeyframe;
     public void SendKeyframeMessage(string message)
     {
-        _Host.GetKeyframeMessage(message);
+        OnReceiveKeyframe(message);
     }
-}
-public interface IKeyframeReceiver
-{
-    public void GetKeyframeMessage(string message);
 }
