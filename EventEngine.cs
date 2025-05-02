218adcbba82563b5596569a23cabcd2b8891382f
KairosFaith <ian@mages.edu.sg>
2025-05-02 17:27:43 +0800

    Update EventEngine.cs

--- EventEngine.cs
+++ EventEngine.cs
@@ -1,6 +1,6 @@
 using System.Collections.Generic;
 public delegate void GameEvent(object sender, params object[] args);
-public delegate void CommonGameEvent(object sender, EventType type, params object[] args);
+public delegate void CommonGameEvent(EventType type, object sender, params object[] args);
 public static class EventEngine
 {
     static Dictionary<EventType, GameEvent> _Events = new Dictionary<EventType, GameEvent>();
@@ -23,7 +23,7 @@ public static class EventEngine
     {
         if (_Events.TryGetValue(type, out GameEvent gameEvent))
             gameEvent?.Invoke(sender, args);
-        OnBroadcastEvent?.Invoke(sender, type, args);
+        OnBroadcastEvent?.Invoke(type, sender, args);
     }
 }
 public enum EventType
