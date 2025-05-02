33f2b6265a13a0463e6b4d317d13f042009a6344
KairosFaith <ian@mages.edu.sg>
2025-05-02 17:15:58 +0800

    Update EventEngine.cs
    added CommonGameEvent to receive all broadcasts

--- EventEngine.cs
+++ EventEngine.cs
@@ -1,29 +1,32 @@
 using System.Collections.Generic;
 public delegate void GameEvent(object sender, params object[] args);
-public static class EventEngine 
+public delegate void CommonGameEvent(object sender, EventType type, params object[] args);
+public static class EventEngine
 {
-    static Dictionary<EventKey, GameEvent> _Events = new Dictionary<EventKey, GameEvent>();
-    public static void SubscribeEvent(EventKey key, GameEvent toAdd)
+    static Dictionary<EventType, GameEvent> _Events = new Dictionary<EventType, GameEvent>();
+    public static CommonGameEvent OnBroadcastEvent;
+    public static void SubscribeEvent(EventType type, GameEvent toAdd)
     {
-        if (_Events.TryGetValue(key, out GameEvent existing))
+        if (_Events.TryGetValue(type, out GameEvent existing))
             existing += toAdd;
         else
-            _Events.Add(key, toAdd);
+            _Events.Add(type, toAdd);
     }
-    public static void UnsubscribeEvent(EventKey key, GameEvent toRemove)
+    public static void UnsubscribeEvent(EventType type, GameEvent toRemove)
     {
-        if (_Events.TryGetValue(key, out GameEvent existing))
+        if (_Events.TryGetValue(type, out GameEvent existing))
             existing -= toRemove;
         if (existing == null)
-            _Events.Remove(key);
+            _Events.Remove(type);
     }
-    public static void BroadcastEvent(EventKey key, object sender, params object[] args)
+    public static void BroadcastEvent(EventType type, object sender, params object[] args)
     {
-        if (_Events.TryGetValue(key, out GameEvent gameEvent))
+        if (_Events.TryGetValue(type, out GameEvent gameEvent))
             gameEvent?.Invoke(sender, args);
+        OnBroadcastEvent?.Invoke(sender, type, args);
     }
 }
-public enum EventKey
+public enum EventType
 {
 
 }
